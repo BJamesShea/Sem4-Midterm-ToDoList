1f2b28b246b41faed679c65970b76c3b7c01cc01
Brandon Shea <brandon.shea1994@icloud.com>
2025-02-26 18:13:04 -0330

    ToDoListManager starting implementation

---
+++ src/main/java/com/keyin/todo/model/ToDoListManager.java
@@ -0,0 +1,58 @@
+package com.keyin.todo.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class ToDoListManager {
+    private List<User> users;
+
+    public ToDoListManager() {
+        this.users = new ArrayList<>();
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public User findUserByName(String name){
+        for (User user : users){
+            if (user.getName().equalsIgnoreCase(name)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void removeUser(String name){
+        Iterator<User> iterator = users.iterator();
+        while(iterator.hasNext()){
+            User user = iterator.next();
+
+            if (user.getName().equalsIgnoreCase(name)){
+                iterator.remove();
+                return;
+            }
+        }
+
+    }
+
+
+    public void displayAllUsers(){
+        if (users.isEmpty()){
+            System.out.println("No users found!");
+            return;
+        }
+
+        for (User user : users){
+            System.out.println(user.getName());
+        }
+    }
+
+}
